d68ce36a49c43039f53ca0746436caf327f8e133
Juno Yu <jxy35@hitit.xyz>
2021-06-12 15:57:36 +0900

    test again

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
-  run(listener)?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
 }
